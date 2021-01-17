590030a8f2288a5ca1894ecc6868d5467d048818
Ahmad Syarifuddin <syarifuddinahmads.me@gmail.com>
2021-01-17 18:23:03 +0700

    start food order

---
+++ app/src/main/java/com/fborder/MainActivity.java
@@ -0,0 +1,50 @@
+package com.fborder;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(HomeFragment.newInstance("", ""));
+    }
+
+    // klik menu bottom
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.navigation_home:
+                    openFragment(HomeFragment.newInstance("",""));
+                    return true;
+                case R.id.navigation_transaction:
+                    openFragment(TransactionFragment.newInstance("",""));
+                    return true;
+                case R.id.navigation_account:
+                    openFragment(AccountFragment.newInstance("",""));
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void openFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.container,fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+}
