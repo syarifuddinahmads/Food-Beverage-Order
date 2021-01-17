f3c461b19b87530c17f1ef1cc0f7b73499ad1d95
Ahmad Syarifuddin <syarifuddinahmads.me@gmail.com>
2021-01-17 19:28:13 +0700

    bottom navigation bar

--- app/src/main/java/com/fborder/MainActivity.java
+++ app/src/main/java/com/fborder/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
         openFragment(HomeFragment.newInstance("", ""));
