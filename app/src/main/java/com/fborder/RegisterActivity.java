7e05893b33c47ac618dd1f7fe06692e360a3ce01
Ahmad Syarifuddin <syarifuddinahmads.me@gmail.com>
2021-01-20 01:18:23 +0700

    login & register page

--- app/src/main/java/com/fborder/RegisterActivity.java
+++ app/src/main/java/com/fborder/RegisterActivity.java
@@ -2,13 +2,48 @@ package com.fborder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private Button btnRegister;
+    private Intent intentLogin;
+    private Intent intentRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // hide app bar
+        getSupportActionBar().hide();
+
+        // init button
+        btnLogin = findViewById(R.id.btn_login);
+        btnRegister = findViewById(R.id.btn_register);
+
+        // init intent
+        intentLogin = new Intent(this,LoginActivity.class);
+        intentRegister =new Intent(this,RegisterActivity.class);
+
+        // action button login
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intentLogin);
+            }
+        });
+
+        // action button register
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
